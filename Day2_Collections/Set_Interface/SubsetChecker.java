a8007e9df807c75f58f9d93f0dbd83f0bc2d00fa
Vijay Kumar <vijay.kumar_cs23@gla.ac.in>
2025-08-06 11:20:26 +0530

    [Vijay Kumar] Set_Interface

---
+++ Day2_Collections/Set_Interface/SubsetChecker.java
@@ -0,0 +1,17 @@
+package Set_Interface;
+
+import java.util.*;
+
+public class SubsetChecker {
+    public static boolean isSubset(Set<Integer> subset, Set<Integer> superset) {
+        return superset.containsAll(subset);
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(1, 2, 3, 4));
+
+        boolean result = isSubset(set1, set2);
+        System.out.println(result);
+    }
+}
---
+++ Day2_Collections/Set_Interface/SymmetricDifference.java
@@ -0,0 +1,24 @@
+package Set_Interface;
+
+import java.util.*;
+
+public class SymmetricDifference {
+    public static Set<Integer> getSymmetricDifference(Set<Integer> set1, Set<Integer> set2) {
+        Set<Integer> result = new HashSet<>(set1);
+        Set<Integer> temp = new HashSet<>(set2);
+
+        result.removeAll(set2);
+        temp.removeAll(set1);
+
+        result.addAll(temp);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Set<Integer> set1 = new HashSet<>(Arrays.asList(1, 2, 3));
+        Set<Integer> set2 = new HashSet<>(Arrays.asList(3, 4, 5));
+
+        Set<Integer> symmetricDifference = getSymmetricDifference(set1, set2);
+        System.out.println(symmetricDifference);
+    }
+}
